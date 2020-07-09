4a617eb243f8d56e34504957d541fc774ac32de0
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-07-09 09:25:39 +0200

    Change icon system to vue-unicons

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,2 +1,3 @@
 import "@/plugins/composition-api";
 import "@/plugins/global-components";
+import "@/plugins/vue-unicons";
---
+++ src/plugins/vue-unicons.js
@@ -0,0 +1,6 @@
+import Vue from "vue";
+import Unicon from "vue-unicons";
+import { uniCrosshair, uniMapMarker } from "vue-unicons/src/icons";
+
+Unicon.add([uniCrosshair, uniMapMarker]);
+Vue.use(Unicon);
