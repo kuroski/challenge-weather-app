22ddb72e224666a590bdf00d544c6b2f60162967
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-07-13 20:43:36 +0200

    Add underscore variables to eslint ignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   },
   rules: {
     "testing-library/await-async-query": "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   },
